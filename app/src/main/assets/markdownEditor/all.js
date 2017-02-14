cd1c8f41b82e34f4db67a30c5a98f8d5b5f95b12
yu juehun <yu juehun>
2017-02-14 13:40:49 +0800

    leanoteN

--- app/src/main/assets/markdownEditor/all.js
+++ app/src/main/assets/markdownEditor/all.js
@@ -13129,6 +13129,7 @@ else
             alt_text = escapeCharacters(attributeEncode(alt_text), "*_[]()");
             url = escapeCharacters(url, "*_");
             var result = "<img src=\"" + url + "\" alt=\"" + alt_text + "\"";
+
             // attacklab: Markdown.pl adds empty title attributes to images.
             // Replicate this bug.
 
